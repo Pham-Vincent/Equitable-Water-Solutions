d10dc839928d69009bce746ee95eadef9989f804
Vincent Pham <vincentpham0000@gmail.com>
2024-03-08 18:12:29 -0500

    API Key + Basic Layout

---
+++ script.js
@@ -0,0 +1,19 @@
+//Gets Google Maps APi Key
+import config from './config.js';
+let map;
+function Load_Map(){
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+  ({key:config.apiKey, v: "weekly"});
+}
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 5,
+  });
+}
+//Calls function to load the map 
+Load_Map();
+//Calls function to details to the map (Markers,Legend,etc)
+initMap();
