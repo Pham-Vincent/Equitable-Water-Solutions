9ca72cbe9140603764c056ec3583cfa0f70fd2fd
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2024-03-12 10:09:15 -0400

    fixed search bar
    search bar now appears top middle of map

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let markersData;
 let markers = []; //stores markers used in search()
 
 function Load_Map(){
-  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"places");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
   ({key:config.apiKey, v: "weekly"});
 }
 
@@ -48,8 +48,10 @@ async function initMap() {
       description: markerInfo.description, 
       graph: markerInfo.graph,
       state: markerInfo.state,
-    });
+    }); 
 
+
+    //adds marker to array, used in search()
     markers.push(marker);
     //infowindow for hover
     const infowindow = new InfoWindow({
@@ -63,7 +65,7 @@ async function initMap() {
     //action for mouse hovering
     marker.addListener('mouseover', () => {
       infowindow.open(map, marker);
-    })
+    });
 
     marker.addListener('mouseout', () => {
       infowindow.close();
@@ -73,6 +75,10 @@ async function initMap() {
       popUpLayer1(marker);
     });
   });
+
+  const input = document.getElementById("search-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 }
 
 //first layer popup
@@ -115,7 +121,6 @@ function openPopup(marker, currentGraph) {
       <p>${marker.description}</p>
       <div id="close-button" onclick="closePopup()">X</div>
     </div>
-    
   `;
   customPopup.style.display = 'block';
   document.getElementById('overlay').style.display = 'block';
@@ -157,11 +162,12 @@ function handleKeyPress(event) {
   }
 }
 
+
 function search() {
-  // Search HTML for element id "search-input" and set user input to lowercase
+  //Search HTML for element id "search-input" and set user input to lowercase
   const searchInput = document.getElementById("search-input").value.toLowerCase();
 
-  // Iterate through each marker
+  //Iterate through each marker
   markers.forEach(marker => {
     const markerTitle = marker.getTitle().toLowerCase();
 
