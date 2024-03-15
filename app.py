0de5b07d312dee751024ef62e53fb0a0919c1922
Vincent Pham <vincentpham0000@gmail.com>
2024-03-15 18:59:43 -0400

    Fixed Bugs
    Fixed Bug Where 2 lines would appear on graph and Fixed bug where y axis was strings instead of floats

--- app.py
+++ app.py
@@ -11,14 +11,17 @@ plt.switch_backend('agg')
 def create_graph():
   years=np.array([2016,2017,2018,2019,2020])
   WithdrawValues = list(request.form.values())
+  WithdrawValues =[float(Floats) for Floats in WithdrawValues] 
   plt.plot(years, WithdrawValues, marker='o',color='g',linestyle='-')
-  plt.title("HEY")
+ 
   plt.xlabel('Years')
   plt.ylabel('Water Withdraw')
+  print(WithdrawValues)
   img = io.BytesIO()
   plt.savefig(img,format="png")
   img.seek(0)
   plot_data = base64.b64encode(img.getvalue()).decode("utf-8")
+  plt.close()
   return jsonify({'src': f'data:image/png;base64,{plot_data}', 'alt': 'Graph'})
   
 @app.route('/',methods=['GET', 'POST'])
