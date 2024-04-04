70494eea70a076f67cb0e9dd70e61a2e2a9a7b55
Will-lamuth <156252007+Will-lamuth@users.noreply.github.com>
2024-04-04 13:47:39 -0400

    Advanced markers
    Replaced the deprecated markers with Advanced Markers. Also added some W comments

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,18 @@
-//searchFunctions.js
+/* 
+title: search.js
+Description: Handles search bar functionality
+*/
 
 export function search(markers, map) {
     const searchInput = document.getElementById("search-input").value.trim().toLowerCase();
   
     markers.forEach(marker => {
-      const markerTitle = marker.getTitle().toLowerCase();
+      const markerTitle = marker.title.toLowerCase();
   
+      //currently zooms and centers on marker. opens infowindow to highlight
       if (markerTitle.includes(searchInput)) {
         console.log("Match found!");
-        map.panTo(marker.getPosition());
+        map.panTo(marker.position);
         map.setZoom(15);
         popUpLayer1(marker, map);
         window.popupLayerOpen = true;
