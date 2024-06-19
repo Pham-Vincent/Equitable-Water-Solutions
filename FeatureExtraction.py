8a7cfdf7a5b869c503c55046346b0e6b4f3f8cd0
Vincent Pham <vincentpham0000@gmail.com>
2024-06-19 19:11:19 -0400

    Fixed The HeatMap For The Graph Now it Works Perfectly Fine

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -7,14 +7,10 @@ from scipy.interpolate import interp1d
 
 def interpolation(DataFrame):
 
-  f = interpolation(DataFrame['Time'],DataFrame['Salinity'],kind='linear', fill_value="extrapolate")
-  interpolated_time = pd.Series(dataframe['Time'])
 
-  interpolated_salinity = f(interpolated_time)
-
-  # Return a new DataFrame with interpolated values
-  interpolated_df = pd.DataFrame({'Time': interpolated_time, 'Interpolated_Salinity': interpolated_salinity})
-  print(interpolated_df)
+  DataFrame['Salinity'] = DataFrame['Salinity'].interpolate(method='linear')
+  print(DataFrame)
+  return DataFrame
 
 
 
@@ -23,6 +19,9 @@ def AverageDailySalinity(DataFrame):
 
   Date = pd.DataFrame(columns=['Date_Reformatted'])
 
+  NewDates=pd.DataFrame(columns=['Time'])
+
+  NewDates['Time'] = DataFrame['Time']
   #Reformats The Time So There is no Hours/Minutes/Seconds
   Date['Date_Reformatted'] = DataFrame['Time'].dt.strftime('%Y-%m-%d')
 
@@ -38,18 +37,22 @@ def AverageDailySalinity(DataFrame):
   #Iterates Through Dates and Then Gets Average Salinity For Each Day  
   for i in range(len(Date)):
     CurrentDate = Date.loc[i,"Date_Reformatted"]
-
+    
     #Gets all Salinity Values That are in a Certain Date 
     filtered_df = DataFrame[DataFrame['Time'].dt.date == CurrentDate.date()]
     #Converts Salinity DF to List With Float Variables
-
     Salinity_List = filtered_df['Salinity'].astype(float).values.tolist()
-    #Gets The Average of the List
-    Date.loc[i,'AvgSalinity'] = np.mean(Salinity_List)
-  Date.columns=['Time','Salinity']
-  #interpolation(Date)
 
-  return Date
+    CurrentDate = pd.to_datetime(CurrentDate, format='%Y-%m-%d')  
+    
+    NewDates.loc[NewDates['Time'] == CurrentDate, 'Salinity'] = np.mean(Salinity_List)   
+    
+  print(NewDates)
+  #Date.columns=['Time','Salinity']
+  
+  
+  
+  return interpolation(NewDates)
 
 
 
