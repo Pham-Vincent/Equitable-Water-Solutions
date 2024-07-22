ee1433495de7719731430635b9f7f19fa264a4da
Vincent Pham <vincentpham0000@gmail.com>
2024-07-21 20:09:08 -0400

    Created HeatMap For Salinity Values

--- GraphingMultiple.py
+++ GraphingMultiple.py
@@ -6,22 +6,58 @@ from datetime import datetime
 from plotly import graph_objs as go
 import numpy as np
 
+
 def AverageDailySalinity(DataFrame):
 
   #Reformats The Time So We can filter by months
-  DataFrame['Time'] = DataFrame['Time'].dt.strftime('%m')
+  DataFrame['Time'] = DataFrame['Time'].dt.strftime('%b')
   DataFrame = DataFrame.drop(columns=['PermitNumber'])
   DataFrame=DataFrame.drop(8771)
-  grouped_df = DataFrame.groupby('Time').mean().reset_index()
+  grouped_df = DataFrame.groupby('Time',as_index=False).mean().reset_index()
   return(grouped_df)
 
   
-
+custom_colorscale = [
+    [0.0, '#9597c2'],    # Lightest blue
+    [0.2, '#5b60b0'],   # Lighter blue
+    [0.4, '#474b96'],   # Light blue
+    [0.6, '#1d1f80'],   # Dark blue
+    [0.8, '#0f1180'],   # Blue
+    [1.0, '#030582']   # Dark blue
+]
 config = {'displaylogo': False,}
 df = pd.read_csv('static\csv\AA2003S009(05) (1).csv')
 df['Time'] = pd.to_datetime(df['Time'])
-df=AverageDailySalinity(df)
 
+df=AverageDailySalinity(df)
+df = df[['Time', 'Depth:0', 'Depth:5', 'Depth:10', 'Depth:15', 'Depth:20', 'Depth:25', 'Depth:30']]
+print(df)
+df = df.transpose()
+
+df = df[[4,3,7,0,8,6,5,1,11,10,9,2]]
+
+new_index = df.iloc[0]
+df = df[1:]
+df.index=[0,5,10,15,20,25,30]
+df.columns = new_index
+fig = px.imshow(df, color_continuous_scale=custom_colorscale, aspect="auto")
+fig.update_layout(
+    yaxis_title='Depth',
+    coloraxis_colorbar=dict(
+        title='(dnɐsuoɥꞱ ɹǝԀ sʇɹɐԀ) ꞱԀԀ',  # Title for the color bar
+        title_side='right'                   # Position the title at the top
+    )
+)
+
+fig.update_traces(
+    hovertemplate='<br>Month: %{x}<br>Depth: %{y}<br>Salinity: %{z}<extra></extra>',
+
+)
+
+fig.show()
+
+
+""" 
 fig = go.Figure()
 
 depths = [30, 25, 20, 15, 10, 5, 0] 
@@ -43,4 +79,4 @@ fig.update_traces(hovertemplate='Salinity Level:%{y}')
 
 # Show the plot
 fig.show()
-  
+   """
