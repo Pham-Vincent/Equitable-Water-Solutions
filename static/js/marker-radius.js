5979a96c031ff383d80c5ff1503bb15c09229cff
Nicholas Gammel <japandude101@gmail.com>
2024-08-06 15:36:27 -0400

    Added Radius to Each Marker to show general location

---
+++ static/js/marker-radius.js
@@ -0,0 +1,26 @@
+//initializes the marker circle to null
+export let markerCircle = null;
+
+export function addCircle (map, marker) {
+    //When hovering, creates circle of area where marker point exists
+    if(markerCircle === null){
+        markerCircle = new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map,
+            center: marker.position,
+            radius: 1610*2,
+          });
+    }
+  };
+
+export function removeCircle (map, marker) {
+    //Hides marker circle from the map and then sets back to null
+    if(markerCircle){
+      markerCircle.setMap(null);
+      markerCircle = null;
+    }
+  }
