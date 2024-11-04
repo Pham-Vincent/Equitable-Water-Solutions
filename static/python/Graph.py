05520f1ecaf51c127cd2e9409353b14174ec8409
Vincent Pham <vincentpham0000@gmail.com>
2024-11-03 22:04:51 -0500

    Refactored the Python Code

--- static/python/Graph.py
+++ static/python/Graph.py
@@ -23,6 +23,8 @@ from plotly import graph_objs as go
 from flask import jsonify
 
 
+#                            Maryland  Graphs 
+#<-------------------------------------------------------------------------------------->
 #This Function is used to create the graph for Maryland
 def Maryland_Tidal_Graph(marker_title,Salinity_df):
 
@@ -32,6 +34,9 @@ def Maryland_Tidal_Graph(marker_title,Salinity_df):
   #creates The colorscale for Graph Currently Blue -> Red 
   color_scale = px.colors.diverging.Portland
 
+
+#                       Creating the Graph + Adding the Color Scale
+#<-------------------------------------------------------------------------------------->
   SalinityPlotted = go.Figure(
     data=go.Scatter(
         x=Salinity_df['Time'],
@@ -46,72 +51,65 @@ def Maryland_Tidal_Graph(marker_title,Salinity_df):
         hovertemplate='Time: %{x}<br>Salinity: %{y} ppt',
         name=""
     ),
-layout=go.Layout(
-    width=800,
-    height=550,
-    xaxis=dict(title="Time"),
-    yaxis=dict(title="Salinity Levels"),
-    showlegend=False
-)
-)
-
-
-
-  SalinityPlotted.update_layout(
-  yaxis_title="Salinity Levels",
-  xaxis_title="Timestamps",
-  title=f"<b>2022 Surface Salinity Information</b>", 
-  title_x=0.7,
-  yaxis_title_font=dict(
-    size=18,
-    family="Roboto"
-      
-    ),
-    xaxis_title_font=dict(
-    size=18,
-    family="Roboto"
-    ), 
-   
-  )
-  
-
-  #This Creates The Time Series Data Slider 
-  SalinityPlotted.update_layout(
-     xaxis=dict(
-        rangeselector=dict(
-            buttons=list([
-              #Creates button on top of graph to pick Specific Time Frames 
-                dict(count=1,
-                     label="1 month",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=3,
-                     label="3 month",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=6,
-                     label="6 month",
-                     step="month",
-                     stepmode="todate"),
-                dict(count=9,
-                     label="9 month",
-                     step="month",
-                     stepmode="backward"),
-                dict(step="all")
-            ])
+    # Define the layout
+    layout=go.Layout(
+        width=800,
+        height=550,
+        xaxis=dict(
+            title="Timestamps",
+            title_font=dict(
+                size=18,
+                family="Roboto"
+            ),
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1 month",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3 months",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6 months",
+                         step="month",
+                         stepmode="todate"),
+                    dict(count=9,
+                         label="9 months",
+                         step="month",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
         ),
-        rangeslider=dict(
-            visible=True
+        yaxis=dict(
+            title="Salinity Levels",
+            title_font=dict(
+                size=18,
+                family="Roboto"
+            )
         ),
-        type="date"
+        title={
+            'text': "<b>2022 Surface Salinity Information</b>",
+            'x': 0.7
+        },
+        showlegend=False
     )
 )
 
-
+  
+        
   graph_html = pio.to_html(SalinityPlotted, full_html=False,config=config)
   graph_json='<div id="graph_html">' + graph_html + '<div>'
   return jsonify({'graph_json': graph_json,})
 
+#                            Virginia Graphs 
+#<-------------------------------------------------------------------------------------->
 
 #This Function is Used to Graph the Viriginia Tidal Location
 def Virginia_Tidal_Graph(WithdrawValues):
@@ -133,61 +131,37 @@ def Virginia_Tidal_Graph(WithdrawValues):
   WithdrawValues_data = [[datetime.strptime(year, '%Y'), WithdrawValues[i]] for i, year in enumerate(year_strings)]
   WithdrawValues_df = pd.DataFrame(WithdrawValues_data,columns=['Years','WaterWithdraw'])
   
-  #Creates a Scatter Plot 
   WithdrawPlotted = px.line(
     data_frame=WithdrawValues_df,
     y='WaterWithdraw',
     x='Years',
-    orientation="v",
     width=700,
     height=550,
-    markers = True,
-      
-)
-  WithdrawPlotted.update_layout(
+    markers=True
+).update_layout(
     title="Water Withdrawal Per Year", 
     title_x=0.5,
     title_font_family="Times New Roman",
-  )
-  #Visual Changes 
-  WithdrawPlotted.update_traces(marker_size=10,marker_color='red',line_color='black')
-  WithdrawPlotted.update_xaxes(title_font_family="Times New Roman")
-
-  #Adding Slider Window And Buttons on top of the graph
-  WithdrawPlotted.update_layout(
     xaxis=dict(
+        title_font_family="Times New Roman",
         rangeselector=dict(
             buttons=list([
-                dict(count=1,
-                     label="1y",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=2,
-                     label="2y",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=3,
-                     label="3y",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=4,
-                     label="4y",
-                     step="year",
-                     stepmode="todate"),
-                 dict(count=1,
-                     label="YTD",
-                     step="year",
-                     stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="todate"),
+                dict(count=2, label="2y", step="year", stepmode="todate"),
+                dict(count=3, label="3y", step="year", stepmode="todate"),
+                dict(count=4, label="4y", step="year", stepmode="todate"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
                 dict(step="all")
             ])
         ),
-        rangeslider=dict(
-            visible=True
-        ),
+        rangeslider=dict(visible=True),
         type="date"
     )
-  )
-
+).update_traces(
+    marker_size=10,
+    marker_color='red',
+    line_color='black'
+)
 
 
 
@@ -249,9 +223,7 @@ def MultiDepthGraphing(marker_title,Depth_df):
         size=16,
         family="Roboto",
       ),
-  )
-
-  fig.update_traces(
+  ).update_traces(
       hovertemplate='<br>Month: %{x}<br>Depth: %{y}<br>Salinity: %{z}<extra></extra>',
 
   )
